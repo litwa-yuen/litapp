5b23b8b7537155df032768c36d2b69a86419a8a5
mogulcorp <yiran@staymogul.com>
2021-07-23 16:58:07 -0700

    add effect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    SwiperModule,
     BrowserModule,
     AppRoutingModule
   ],
